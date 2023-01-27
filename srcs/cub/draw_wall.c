1ebba9c7c6e8fecd152dc2e2b7ea9ac01a58d9c2
Louis Durieux <ldurieux@student.42lyon.fr>
2023-01-27 18:15:41 +0100

    fix draw wall

--- srcs/cub/draw_wall.c
+++ srcs/cub/draw_wall.c
@@ -12,6 +12,7 @@
 
 #include "cub.h"
 
+#include <stdio.h>
 void	cub_draw_wall(t_map *map, t_paint *paint, t_wall wall)
 {
 	int			img_height;
@@ -21,7 +22,7 @@ void	cub_draw_wall(t_map *map, t_paint *paint, t_wall wall)
 	uint16_t	y_end;
 
 	if (wall.face_idx < 0 || wall.face_idx > 3 || wall.face_x < 0.0f
-		|| wall.face_x > 1.0f || wall.height < 0.0f || wall.height > 1.0f)
+		|| wall.face_x > 1.0f || wall.height < 0.0f)
 		return ;
 	img_height = map->textures[wall.face_idx].height;
 	img = (map->textures + wall.face_idx);
@@ -30,9 +31,16 @@ void	cub_draw_wall(t_map *map, t_paint *paint, t_wall wall)
 	w_pos.x = wall.draw_x;
 	y_end = (WIN_HEIGHT + WIN_HEIGHT * wall.height) / 2;
 	w_pos.y = WIN_HEIGHT - y_end - 1;
+	if (w_pos.y < 0)
+		w_pos.y = -1;
 	while (++w_pos.y < y_end)
 	{
-		i_pos.y = img_height * w_pos.y / WIN_HEIGHT;
+		if (w_pos.y >= WIN_HEIGHT)
+			break ;
+		i_pos.y = img_height * ((float)(w_pos.y - (WIN_HEIGHT - y_end))
+			/ (WIN_HEIGHT * wall.height));
+		if (i_pos.y > 10 || i_pos.y < 0)
+			continue ;
 		paint->data[w_pos.y * paint->width + w_pos.x] = img->data[i_pos.y
 			* img->width + i_pos.x];
 	}
